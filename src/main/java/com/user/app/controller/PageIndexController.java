377b4a7ce1dd8c6d30c6497e6e90f4f9ad8c277d
zengqingcai@feitaikeji.com <zengqingcai@feitaikeji.com>
2020-03-03 16:32:39 +0800

    添加静态资源

---
+++ src/main/java/com/user/app/controller/PageIndexController.java
@@ -0,0 +1,15 @@
+package com.user.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Authod:zeng
+ * @Date: 2020/3/3 15:56
+ * @Version: 0.0.1
+ * 所有页面的跳转
+ */
+@Controller
+@RequestMapping(value = "/")
+public class PageIndexController {
+}
